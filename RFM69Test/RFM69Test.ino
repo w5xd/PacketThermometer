088927c2b3f67901ed1ce4438c7aabff2bf391b3
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2025-08-19 15:41:53 +0000

    scope test

--- RFM69Test/RFM69Test.ino
+++ RFM69Test/RFM69Test.ino
@@ -7,6 +7,8 @@
     // Create a library object for our RFM69HCW module:
     RFM69 radio(RFM69_CHIP_SELECT_PIN, RFM69_INT_PIN, true);
 
+    static bool radioOK = false;
+
 void setup()
 {
     Serial.begin(9600);
@@ -15,11 +17,11 @@ void setup()
     digitalWrite(RFM69_CHIP_SELECT_PIN, HIGH);
     SPI.begin();
 
-    auto ok = radio.initialize(RF69_915MHZ, 127, 1);
+    radioOK = radio.initialize(RF69_915MHZ, 127, 1);
     Serial.print("radio.initialize: "); 
-    Serial.println(static_cast<int>(ok));
+    Serial.println(static_cast<int>(radioOK));
 
-    if (ok)
+    if (radioOK)
     {
         auto f = radio.getFrequency();
         Serial.print("default frequency: ");
@@ -37,4 +39,45 @@ void setup()
 
 void loop()
 {
+    auto now = millis();
+    static auto testStartTime = now;
+    static enum class State_t {IDLE, SCOPE_LOOP} state = State_t::IDLE;
+    if (radioOK)
+    {
+        if (Serial.available() > 0)
+        {
+            char input = Serial.read();
+            switch (input)
+            {
+                case 'T':
+                case 't':
+                    if (state == State_t::IDLE)
+                        state = State_t::SCOPE_LOOP;
+                    break;
+                case 'C':
+                case 'c':
+                    if (state == State_t::SCOPE_LOOP)
+                        state = State_t::IDLE;
+                    break;
+                default:
+                    Serial.println('?');
+            }
+        }
+
+        switch (state)
+        {
+            case State_t::SCOPE_LOOP:
+                static const int LOOP_INTERVAL_MSEC = 20;
+                if (now - testStartTime >= LOOP_INTERVAL_MSEC)
+                {
+                    testStartTime = now;
+                    radio.setFrequency(920000000);
+                    radio.getFrequency();
+                }
+                break;
+            case State_t::IDLE:
+            default:
+                break;
+        }
+    }
 }
